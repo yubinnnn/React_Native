22bd35274e1fab4d99102a67a37f89b8ee386c4d
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-04-12 20:31:04 +0900

    Add files via upload

---
+++ react-native-component/src/component/EventInput.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const EventInput = () => {
+    const [text, setText] = useState('');
+    const _onChange = event => setText(event.nativeEvent.text)
+
+    return (
+        <View>
+            <Text style={{ margin: 10, fontSize: 30}}>text: {text}</Text>
+            <TextInput
+            style={{ borderWidth: 1, padding: 10, fontSize: 20}}
+            placeholder="Enter a text..."
+            onChange={_onChange}
+            />
+        </View>
+    );
+};
+
+export default EventInput
---
+++ react-native-component/src/component/MyButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+
+const MyButton = props => {
+    console.log(props)
+    return (
+        <Pressable
+        style={{
+            backgroundColor: '#3498db',
+            padding: 16,
+            margin: 10,
+            borderRadius: 8,
+        }}
+        onPress={() => props.onPress()}
+        >
+            <Text style={{ color: 'white', fontSize: 24 }}>
+            {props.children || props.title}
+            </Text>
+        </Pressable>
+    );
+};
+
+MyButton.defaultProps = {
+    title: 'Button',
+};
+
+export default MyButton;
