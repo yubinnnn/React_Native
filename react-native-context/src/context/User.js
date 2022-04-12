26c39073bfb404d1e32cf1930326f18e929ed0b3
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-04-12 20:34:07 +0900

    Add files via upload

---
+++ react-native-context/src/context/User.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+const UserContext = createContext({ 
+    user: { name: ''},
+    dispatch: () => {},
+});
+
+const UserProvider = ({ children }) => {
+    const [name, setName] = useState('Yubin Lee');
+
+    const value = { user: {name}, dispatch: setName};
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
+
+const UserConsumer = UserContext.Consumer;
+
+export { UserProvider, UserConsumer};
+export default UserContext;
