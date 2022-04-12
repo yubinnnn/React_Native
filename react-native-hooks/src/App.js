d99bdfa2aaf9d577fffd8cba9946fdcad54db068
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-04-12 20:35:52 +0900

    Add files via upload

---
+++ react-native-hooks/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import Form from './components/Form';
+import Button from './components/Button';
+import Length from './components/Length';
+import Dog from './components/Dog';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #fff;
+    justify-content: center;
+    align-items: center;
+`;
+
+const App = () => {
+    return (
+    <Container>
+        <Dog />
+    </Container>
+    );
+};
+
+export default App;
